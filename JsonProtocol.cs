42d5d297a35c6e2dd9891f78cdfbe50a99e87683
GeorgeKalash <george.kalash@softmachine.co>
2020-10-17 18:45:50 +0300

    removed-report-class

---
+++ JsonProtocol.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SharedClasses
+{
+    public class JsonProtocol
+    {
+        public class QryStructure<T>
+        {
+            public int count;
+            public List<T> list;
+        }
+        public class GetStructure<T>
+        {
+            public T record;
+            public int statusId;
+            public string message;
+        }
+    }
+}
