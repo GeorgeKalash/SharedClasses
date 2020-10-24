60eea3714444f3894017bf0adc943935d9902ed4
GeorgeKalash <GeorgeKalash@icloud.com>
2020-10-24 11:57:38 +0300

    added-xml-store

--- XMLTools.cs
+++ XMLTools.cs
@@ -4,9 +4,163 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Xml;
+using System.Xml.Serialization;
 
 namespace SharedClasses
 {
+    public class XMLSerializer
+    {
+        /// <summary>
+        /// populate a class with xml data 
+        /// </summary>
+        /// <typeparam name="T">Object Type</typeparam>
+        /// <param name="input">xml data</param>
+        /// <returns>Object Type</returns>
+        public T Deserialize<T>(string input) where T : class
+        {
+            System.Xml.Serialization.XmlSerializer ser = new System.Xml.Serialization.XmlSerializer(typeof(T));
+
+            using (StringReader sr = new StringReader(input))
+            {
+                return (T)ser.Deserialize(sr);
+            }
+        }
+
+        /// <summary>
+        /// convert object to xml string
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="ObjectToSerialize"></param>
+        /// <returns></returns>
+        public string Serialize<T>(T ObjectToSerialize)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(ObjectToSerialize.GetType());
+
+            using (StringWriter textWriter = new StringWriter())
+            {
+                xmlSerializer.Serialize(textWriter, ObjectToSerialize);
+                return textWriter.ToString();
+            }
+        }
+
+        public T DeserializeFromFile<T>(string path) where T : class
+        {
+            string xmlInputData = File.ReadAllText(path);
+            return Deserialize<T>(xmlInputData);
+        }
+
+        public void SerializeToFile<T>(T ObjectToSerialize, string path) where T : class
+        {
+            string xmlText = Serialize<T>(ObjectToSerialize);
+            File.WriteAllText(path, xmlText);
+        }
+
+    }
+    public class XMLStore
+    {
+        const string KEY = "key";
+        const string VALUE = "value";
+        public class KeyValue
+        {
+            public int key;
+            public string value;
+            public KeyValue(int _key, string _value)
+            {
+                key = _key;
+                value = _value;
+            }
+
+            public object identityKey()
+            {
+                return key;
+            }
+        }
+
+        public static string keyValue(List<KeyValue> _list, ref short _listHead, int _Id)
+        {
+            while (_listHead < _list.Count)
+            {
+                if (_list[_listHead].key == _Id)
+                    return _list[_listHead].value;
+                ++_listHead;
+            }
+            return null;
+        }
+
+        public static string keyValue(List<KeyValue> _list, int _Id)
+        {
+            short _ = 0;
+            return keyValue(_list, ref _, _Id);
+        }
+
+        public static XmlReader read(string _filePath)
+        {
+            return XmlReader.Create(_filePath);
+        }
+
+        public static string keyValue(string _filePath, int _key, short _languageId)
+        {
+            if (_languageId == 0)
+                _languageId = 1;
+
+            XmlReader reader = read(_filePath);
+            string value = null;
+
+            if (reader.ReadToDescendant(string.Format("L{0}", _languageId)))
+            {
+                int depth = reader.Depth;
+                while (reader.Read() && reader.Depth > depth)
+                {
+                    if (reader.IsStartElement())
+                    {
+                        int key;
+                        if (Int32.TryParse(reader[KEY], out key) && key == _key)
+                        {
+                            value = reader[VALUE];
+                            break;
+                        }
+                    }
+                }
+            }
+                
+            reader.Close();
+            return value;
+        }
+
+        public static List<KeyValue> xmlToList(string _filePath, string _groupId)
+        {
+            List<KeyValue> list = new List<KeyValue>();
+            XmlReader reader = read(_filePath);
+
+            if (reader.ReadToDescendant(_groupId))
+            {
+                int depth = reader.Depth;
+                while (reader.Read() && reader.Depth > depth)
+                {
+                    if (reader.IsStartElement())
+                    {
+                        int key;
+                        if (Int32.TryParse(reader[KEY], out key))
+                        {
+                            list.Add(new KeyValue(key, reader[VALUE]));
+                        }
+                    }
+                }
+            }
+
+            reader.Close();
+
+            return list;
+        }
+
+        public static List<KeyValue> xmlToList(string _filePath, short _languageId)
+        {
+            if (_languageId == 0)
+                _languageId = 1;
+            string groupId = string.Format("L{0}", _languageId);
+            return xmlToList(_filePath, groupId);
+        }
+    }
     public class XMLTools
     {
         private static XmlReader read(string _filePath)
