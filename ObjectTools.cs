23a10522e8ca78c2b8d4b0bf38c8c6cb424e482c
GeorgeKalash <GeorgeKalash@icloud.com>
2020-12-10 16:41:53 +0200

    wip

--- ObjectTools.cs
+++ ObjectTools.cs
@@ -7,8 +7,29 @@ using System.ComponentModel.DataAnnotations;
 
 namespace SharedClasses
 {
+    public static class CloneClass
+    {
+        /// <summary>
+        /// Clones a object via shallow copy
+        /// </summary>
+        /// <typeparam name="T">Object Type to Clone</typeparam>
+        /// <param name="obj">Object to Clone</param>
+        /// <returns>New Object reference</returns>
+        public static T CloneObject<T>(this T obj) where T : class
+        {
+            if (obj == null) return null;
+            System.Reflection.MethodInfo inst = obj.GetType().GetMethod("MemberwiseClone",
+                System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
+            if (inst != null)
+                return (T)inst.Invoke(obj, null);
+            else
+                return null;
+        }
+    }
+
     public static class ObjectDataHelper
     {
+
         public static List<object> typedToObject<T>(List<T> list)
         {
             List<object> returnedData = new List<object>();
