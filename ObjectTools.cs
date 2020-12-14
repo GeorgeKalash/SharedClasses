70c3a0e7b5d0805550a92325d672f273e884beb2
GeorgeKalash <GeorgeKalash@icloud.com>
2020-12-14 09:38:59 +0200

    wip

--- ObjectTools.cs
+++ ObjectTools.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Reflection;
 using System.Globalization;
 using System.ComponentModel.DataAnnotations;
+using System.IO;
 
 namespace SharedClasses
 {
+
     public static class CloneClass
     {
         /// <summary>
@@ -24,6 +26,28 @@ namespace SharedClasses
                 return (T)inst.Invoke(obj, null);
             else
                 return null;
+        }
+
+        public static T Clone<T>(T source)
+        {
+            if (!typeof(T).IsSerializable)
+            {
+                throw new ArgumentException("The type must be serializable.", "source");
+            }
+
+            if (Object.ReferenceEquals(source, null))
+            {
+                return default(T);
+            }
+
+            System.Runtime.Serialization.IFormatter formatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+            Stream stream = new MemoryStream();
+            using (stream)
+            {
+                formatter.Serialize(stream, source);
+                stream.Seek(0, SeekOrigin.Begin);
+                return (T)formatter.Deserialize(stream);
+            }
         }
     }
 
