a23973e173d8f7dcad8c06b6868e96d1e8139ead
George Kalash <georgekalash@icloud.com>
2023-07-01 11:28:29 +0300

    gen-key

---
+++ KeyGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedClasses
+{
+    public static class KeyGenerator
+    {
+        public static double generate(short _length)
+        {
+            double n = 0;
+            for (int i = _length - 1; i >= 0; i--)
+            {
+                Random random = new Random();
+                int oneDigitNumber = random.Next(0, 10);
+                n += oneDigitNumber * Math.Pow(10, i);
+            }
+
+            return n;
+        }
+    }
+
+}
