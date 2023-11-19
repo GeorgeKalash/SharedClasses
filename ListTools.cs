b133cc2c9201b0be60297a09983ffc375a409fa9
GeorgeKalash <GeorgeKalash@icloud.com>
2023-11-19 17:46:46 +0200

    litstools

---
+++ ListTools.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedClasses
+{
+    public static class ListTools
+    {
+        public static List<int> RemoveDuplicates(List<int> inputList)
+        {
+            // Use a HashSet to keep track of unique values
+            HashSet<int> uniqueValues = new HashSet<int>();
+
+            // Create a new list to store the result without duplicates
+            List<int> resultList = new List<int>();
+
+            foreach (int value in inputList)
+            {
+                // If the value is not in the HashSet, add it to both the HashSet and the result list
+                if (uniqueValues.Add(value))
+                {
+                    resultList.Add(value);
+                }
+            }
+
+            return resultList;
+        }
+    }
+}
