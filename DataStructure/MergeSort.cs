7ef1e715f58da683aa5df9242f903840669063e3
unknown <fawzy.mestrah@softmachine.co>
2024-08-22 11:12:27 +0300

    ..

--- DataStructure/MergeSort.cs
+++ DataStructure/MergeSort.cs
@@ -47,5 +47,48 @@ namespace DataStructure
             for (int l = 0; l < temp.Count; l++)
                 list[left + l] = temp[l];
         }
+
+
+        // Merge Sort with delegate
+
+        public static void sort<T>(List<T> list, Comparison<T> comparison)
+        {
+            mergeSort(list, 0, list.Count - 1, comparison);
+        }
+        private static void mergeSort<T>(List<T> list, int left, int right,Comparison<T> comparison) 
+        {
+            if (left >= right)
+                return;
+
+            int mid = (left + right) / 2;
+            mergeSort(list, left, mid, comparison);
+            mergeSort(list, mid + 1, right, comparison);
+            merge(list, left, mid, right, comparison);
+        }
+
+        private static void merge<T>(List<T> list, int left, int mid, int right, Comparison<T> comparison) 
+        {
+            List<T> temp = new List<T>(right - left + 1);
+            int i = left;
+            int j = mid + 1;
+
+            while (i <= mid && j <= right)
+            {
+                if (comparison(list[i],list[j]) <= 0)
+                    temp.Add(list[i++]);
+                else
+                    temp.Add(list[j++]);
+            }
+
+            while (i <= mid)
+                temp.Add(list[i++]);
+
+            while (j <= right)
+                temp.Add(list[j++]);
+
+            for (int l = 0; l < temp.Count; l++)
+                list[left + l] = temp[l];
+        }
+
     }
 }
